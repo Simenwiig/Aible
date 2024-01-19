00df0275c59ecf866d62f12d46864c73527d6263
Simenwiig2 <simenwiig0@gmail.com>
2024-01-19 16:22:06 +0100

    Trun of and on webcam

--- Assets/MediaPipe/Scripts/Visualizers/WebCamVisualizer.cs
+++ Assets/MediaPipe/Scripts/Visualizers/WebCamVisualizer.cs
@@ -5,11 +5,34 @@ using UnityEngine.UI;
 
 public class WebCamVisualizer : MonoBehaviour
 {
+    [SerializeField] bool showCam = true;
     [SerializeField] WebCamInput webCamInput;
     [SerializeField] RawImage webCamImageUI;
 
+    private void Start()
+    {
+        webCamImageUI.gameObject.SetActive(showCam);      
+    }
+
     private void LateUpdate()
     {
+        if (!showCam)
+            return;
+
         webCamImageUI.texture = webCamInput.inputImageTexture;
+    }
+
+    public void ChangeCam()
+    {
+        if (showCam)
+        {
+            showCam = false;
+            webCamImageUI.gameObject.SetActive(false);
+        }
+        else
+        {
+            showCam = true;
+            webCamImageUI.gameObject.SetActive(true);
+        }     
     }
 }
