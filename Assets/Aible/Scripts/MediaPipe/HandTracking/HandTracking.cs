95ded916929e193d2f353446d07c015bcaa235a7
Simenwiig2 <simenwiig0@gmail.com>
2024-03-13 14:27:37 +0100

    Hand is more stable

--- Assets/Aible/Scripts/MediaPipe/HandTracking/HandTracking.cs
+++ Assets/Aible/Scripts/MediaPipe/HandTracking/HandTracking.cs
@@ -16,15 +16,17 @@ namespace Mediapipe.Unity.Sample.HandTracking
         [SerializeField] private GameObject _hand;
         [Range(0.1f, 3f)][SerializeField] private float _handSensitivity = 1f;
 
-        private HandPoints _handParent;
-        public List<NormalizedLandmarkList> _handLandmarks;
-        public List<LandmarkList> _handWorldLandmarks;
+        [HideInInspector] public HandPoints HandParent;
+        public List<NormalizedLandmarkList> HandLandmarks;
+        public List<LandmarkList> HandWorldLandmarks;
+
+        private float _timer;
 
         private void Awake()
         {
             GameObject handClone = GameObject.Instantiate(_hand) as GameObject;
             handClone.name = "Hand";
-            _handParent = handClone.GetComponent<HandPoints>();
+            HandParent = handClone.GetComponent<HandPoints>();
         }
 
         protected override void OnStartRun()
@@ -52,38 +54,60 @@ namespace Mediapipe.Unity.Sample.HandTracking
         private void OnHandWorldLandmarksOutput(object stream, OutputStream<List<LandmarkList>>.OutputEventArgs eventArgs)
         {
             var packet = eventArgs.packet;
-            _handWorldLandmarks = packet == null ? default : packet.Get(LandmarkList.Parser);     
+            HandWorldLandmarks = packet == null ? default : packet.Get(LandmarkList.Parser);
         }
 
         private void OnHandLandmarksOutput(object stream, OutputStream<List<NormalizedLandmarkList>>.OutputEventArgs eventArgs)
         {
             var packet = eventArgs.packet;
-            _handLandmarks = packet == null ? default : packet.Get(NormalizedLandmarkList.Parser);
-        }    
+            HandLandmarks = packet == null ? default : packet.Get(NormalizedLandmarkList.Parser);
+        }
 
         private void Update()
         {
-            if (_handLandmarks == null || _handWorldLandmarks == null || _handParent == null)
+            if (HandLandmarks == null || HandWorldLandmarks == null || HandParent == null)
             {
-                _handParent.gameObject.SetActive(false);
+                if (_timer > 0.1f)
+                {
+                    HandParent.gameObject.SetActive(false);
+                }
+                else
+                {
+                    _timer += Time.deltaTime;
+                }
                 return;
             }
-            _handParent.gameObject.SetActive(true);
 
-            float parentX = _handLandmarks[0].Landmark[0].X - 0.5f;
-            float parentY = 0.8f - _handLandmarks[0].Landmark[0].Y;
+            if (_timer > 0)
+            {
+                _timer = 0;
+            }
+
+            HandParent.gameObject.SetActive(true);
+
+            float handX = HandLandmarks[0].Landmark[5].X - HandLandmarks[0].Landmark[17].X;
+            float handY = HandLandmarks[0].Landmark[5].Y - HandLandmarks[0].Landmark[17].Y;
+            float handZ = HandLandmarks[0].Landmark[5].Z - HandLandmarks[0].Landmark[17].Z;
+
+            float handWidht_pix = MathF.Sqrt(MathF.Pow(handX, 2) + MathF.Pow(handY, 2) + MathF.Pow(handZ, 2));
+
+            float parentX = HandLandmarks[0].Landmark[9].X - 0.5f;
+            float parentY = 0.5f - HandLandmarks[0].Landmark[9].Y;
             float parentZ = 0;
 
-            _handParent.gameObject.transform.localPosition = new Vector3(parentX * 20 * _handSensitivity, 
-                                                                        parentY * 8 * _handSensitivity, parentZ);
+            HandParent.gameObject.transform.localPosition = new Vector3(parentX * 25 * _handSensitivity,
+                                                            parentY * 15 * _handSensitivity, parentZ);
 
-            for (int i = 0; i < _handParent._HandPoints.Count; i++)
+            for (int i = 0; i < HandParent._HandPoints.Count; i++)
             {
-                float x = _handWorldLandmarks[0].Landmark[i].X * 20;
-                float y = _handWorldLandmarks[0].Landmark[i].Y * 20;
-                float z = _handWorldLandmarks[0].Landmark[i].Z * 20;
+                float x = HandWorldLandmarks[0].Landmark[i].X * 20;
+                float y = HandWorldLandmarks[0].Landmark[i].Y * 20;
+                float z = HandWorldLandmarks[0].Landmark[i].Z * 20;
 
-                _handParent._HandPoints[i].transform.localPosition = new Vector3(x, y, z);
+
+            
+                HandParent._HandPoints[i].transform.localPosition = Vector3.Lerp(HandParent._HandPoints[i].transform.localPosition, new Vector3(x, y, z), 15f);
+                
             }
         }
     }
